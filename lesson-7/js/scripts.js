bbe543c0fad07061361dc6dfa65eccca6ae830f9
mnbaldwin@byui.edu <matthew.baldwin@bgrs.com>
2020-02-22 18:56:22 -0700

    check in for gallery

---
+++ lesson-7/js/scripts.js
@@ -0,0 +1,21 @@
+function lastUpdated() {
+  let d = new Date(document.lastModified);
+  document.getElementById("spanLastUpdated").innerHTML = "Last updated: " + d.toLocaleString();
+}
+
+function toggleMenu() {
+  document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+function main() {
+  lastUpdated();
+  toggleBanner();
+  diplayWindChill();
+}
+
+function toggleBanner() {
+  var d = new Date();
+  if (d.getDay() == 5) {
+    document.getElementById("topleftbanner").className = "topleftbanneron";
+  }
+}
